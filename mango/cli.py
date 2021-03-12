3b76c7fed94a563a85a66c0761c69de290af670a
Junyi <xiaojunyi@gmail.com>
2021-03-12 14:22:31 +0800

    cleaned up cli stuff

--- mango/cli.py
+++ mango/cli.py
@@ -1,67 +1,21 @@
+"""This is the first file ran. Manages command line args and 
+other required variables.
+
+These CLI arguments are available (all are optional):
+
+-m : name of manga (str)
+-f : absolute path to folder for download (str)
+-l : manga language (str)
+-s : low quality images (bool)
+-u : mangadex username (str)
+-p : mangadex password (str)
+"""
+
 import argparse
 import os
-from .helpers import safe_mkdir
 from .config import mango_config
 
 print('Welcome to mango!')
-
-
-class SafeArgs:
-
-    def __init__(self, args: argparse.Namespace):
-        for a in args.__dict__:
-            setattr(self, a, args.__dict__[a])
-        self.check_all()
-
-    def check_title(self):
-        if not self.manga:
-            m = input('Search for a manga: ')
-            self.manga = m
-
-    def check_path(self):
-        if not self.folder and not mango_config.get_root_dir():
-            f = input('Please specify directory to store manga in: ')
-            if not os.path.isdir(f):
-                print(f'{f} does not exist. Create directory? - [y/n]')
-                resp = input()
-                if resp.lower() == 'y':
-                    safe_mkdir(f)
-                elif resp.lower() == 'n':
-                    self.check_path()
-                else:
-                    print('Invalid input.')
-                    self.check_path()
-            self.folder = f
-            mango_config.set_root_dir(f)
-        elif not self.folder:
-            self.folder = mango_config.get_root_dir()
-        else:
-            mango_config.set_root_dir(self.folder)
-
-    def check_username(self):
-        if not self.username and not mango_config.get_username():
-            u = input('Mangadex username: ')
-            mango_config.set_username(u)
-        elif not self.username:
-            self.username = mango_config.get_username()
-        else:
-            mango_config.set_username(self.username)
-
-    def check_password(self):
-        if not self.password and not mango_config.get_password():
-            u = input('Mangadex password: ')
-            mango_config.set_password(u)
-        elif not self.password:
-            self.password = mango_config.get_password()
-        else:
-            mango_config.set_password(self.password)
-
-    def check_all(self):
-        self.check_username()
-        self.check_password()
-        self.check_title()
-        self.check_path()
-
 
 argparser = argparse.ArgumentParser()
 # manga title
@@ -87,4 +41,52 @@ argparser.add_argument('-u', '--username', action='store', type=str,
 argparser.add_argument('-p', '--password', action='store', type=str,
                        help='Mangadex password.')
 
-safe_args = SafeArgs(argparser.parse_args())
+args = argparser.parse_args()
+
+
+def check_title(args):
+    if not args.manga:
+        m = input('Search for a manga: ')
+        args.manga = m
+
+
+def check_folder(args):
+    if not args.folder and not mango_config.get_root_dir():
+        f = input('Please specify directory to store manga in: ')
+        if not os.path.isdir(f):
+            print(f'{f} does not exist. Create directory?')
+            print('[y] - yes    [n] - no')
+            resp = input()
+            if resp.lower() == 'y':
+                os.mkdir(f)
+            elif resp.lower() == 'n':
+                args.check_path()
+            else:
+                print('Invalid input.')
+                args.check_path()
+        args.folder = f
+        mango_config.set_root_dir(f)
+    elif not args.folder:
+        args.folder = mango_config.get_root_dir()
+    else:
+        mango_config.set_root_dir(args.folder)
+
+
+def check_username(args):
+    if not args.username and not mango_config.get_username():
+        u = input('Mangadex username: ')
+        mango_config.set_username(u)
+    elif not args.username:
+        args.username = mango_config.get_username()
+    else:
+        mango_config.set_username(args.username)
+
+
+def check_password(args):
+    if not args.password and not mango_config.get_password():
+        u = input('Mangadex password: ')
+        mango_config.set_password(u)
+    elif not args.password:
+        args.password = mango_config.get_password()
+    else:
+        mango_config.set_password(args.password)
