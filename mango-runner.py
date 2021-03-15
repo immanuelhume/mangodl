b30da57b45cb7e628294f7367b4c2fbfcb1bd92e
Junyi <xiaojunyi@gmail.com>
2021-03-16 00:45:24 +0800

    adjusting configs

--- mango-runner.py
+++
@@ -1,4 +0,0 @@
-from mango.mango import main
-
-if __name__ == '__main__':
-    main()
--- mango/config/mango_config.py
+++ mango/config/mango_config.py
@@ -11,6 +11,8 @@ if not os.path.exists(CONFIG_FILE):
     config['links']['api_base'] = 'https://api.mangadex.org/v2/'
     config['links']['search_url'] = 'https://mangadex.org/search?tag_mode_exc=any&tag_mode_inc=all&title='
     config['links']['login_url'] = 'https://mangadex.org/ajax/actions.ajax.php?function=login&nojs=1'
+    config['user info'] = {}
+    config['settings'] = {}
     with open(CONFIG_FILE, 'w') as f:
         config.write(f)
 
