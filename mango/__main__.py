b415e8ab799d186ccbb75695bb5be102b73829f9
Junyi <xiaojunyi@gmail.com>
2021-03-16 15:33:15 +0800

    some more minor docstring edits

--- mango/__main__.py
+++ mango/__main__.py
@@ -1,3 +1,8 @@
+"""
+This file runs when we call mango as a module.
+>>> python3 -m mango
+"""
+
 from .mango import main
 
 main()
--- mango_runner.py
+++ mango_runner.py
@@ -1,3 +1,8 @@
+"""
+Run this file as __main__ to run the whole app.
+VS Code debug mode can be started from here.
+"""
+
 from mango.mango import main
 
 if __name__ == '__main__':
